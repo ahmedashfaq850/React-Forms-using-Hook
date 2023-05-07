e484973a38d745ce1f3fce4769bd558c0f7513f1
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2023-05-07 21:22:25 +0500

    updated

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const Form = () => {
+     // FOrm object
+    const form = useForm()
+
+    // Destructure objects from form
+    const {register, handleSubmit, formState} = form
+
+    // Destructure Error from fromState
+    const {errors} = formState;
+
+    // Form handle logic
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
   return (
     <div>
-      <form>
-        <label htmlFor='username'>Username</label>
-        <input type='text' id="username" name='username'/>
-        
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>  {/* This will prevent the browser to vaildate the data */}
         <label htmlFor='email'>Email</label>
-        <input type="email" name="email" id=""email/>
-
-        <label htmlFor='password'>Password</label>
-        <input type='password' name="password" />
-
+        <input type='text' id="email" placeholder='Enter Email Address' {...register("email", {
+            pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: 'Email is not correct'
+            }
+            /* For normal fields */
+            /* 
+            required: {
+                value: true,
+                message: "Username is required"
+            }
+            */ 
+        })}/>
+        <p>
+            {errors.email?.message}
+        </p>
+        <label htmlFor='email'>Password</label>
+        <input type='password' id="password" placeholder='Enter Your password' {...register("password")}/>
+        <p>
+            {errors.password?.message}
+        </p>
+        <button>Submit</button>
       </form>
     </div>
   )
