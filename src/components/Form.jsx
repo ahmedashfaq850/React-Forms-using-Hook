67138c72501b077672dd5dff542b479ac155ccd5
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2023-05-07 11:44:11 +0500

    update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,7 +10,7 @@ const Form = () => {
         <label htmlFor='email'>Email</label>
         <input type="email" name="email" id=""email/>
 
-        <label htmlFor='password'>password</label>
+        <label htmlFor='password'>Password</label>
         <input type='password' name="password" />
 
       </form>
